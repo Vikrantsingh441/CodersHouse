a2b8158b1a59445559af1cb5b6ef2847b6259436
Vikrant singh <Singvikrant21@gmail.com>
2023-11-27 11:29:29 +0530

    otp-service and verify otp

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -1,15 +1,56 @@
 const otpService = require('../services/otp-service');
+const HashService = require('../services/hash-service');
+const hashService = require('../services/hash-service');
 
 class AuthController {
-    sendOtp(req,res){
+    async sendOtp(req, res) {
         //logic
-        const {phone} = req.body;
-        if(!phone){
+        const { phone } = req.body;
+        if (!phone) {
             return res.status(400).json({ message: 'Phone field is required' });
         }
-        res.json(otpService.generateOtp());
-        // const otp = otpService.generateOtp()
-        //     res.send(otp)
+        const otp = await otpService.generateOtp()
+
+        // hash otp 
+        const ttl = 1000 * 60 * 2
+        const expires = Date.now() + ttl
+        const data = `${phone}.${otp}.${expires}`
+        const hash = hashService.hashOtp(data)
+
+
+        //send otp
+        try {
+            await otpService.sendBySms(phone, otp);
+            res.json({
+                hash: `${hash}.${expires}`,
+                phone:phone
+            })
+
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({ message: 'message sending failed' })
+        }
+        //     res.send(otp) 
+    }
+
+    verifyOtp(req,res){
+        const {otp,hash,phone}=req.body;
+        if(!otp||!hash||!phone){
+            res.status(400).json({message:"All fields are required !"});
+        }
+        const [hashedOtp,expires] = hash.split('.')
+        if (Date.now()>expires){
+            res.status(400).json({message:"Otp expired"})
+        }
+        const data = `${phone}.${otp}.${expires}`
+        const isValid = otpService.verifyOtp(hashedOtp,data)
+        if(!isValid){
+            res.status(400).json({message:'Invalid Otp'})
+        }
+
+        let user
+        let accessToken
+        let refreshToken
     }
 }
 
