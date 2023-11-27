ba66ace8e4945c80ba29ac077a65b09577bb0526
Vikrant <Singvikrant21@gmail.com>
2023-11-27 15:51:00 +0530

    added connection

--- backend/database.js
+++ backend/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 function DbConnect (){
     const DB_URL= process.env.DB_URL;
     // Database connection
-    mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+    mongoose.connect(DB_URL);
 
     const db = mongoose.connection;
     db.on('error',console.error.bind(console,'connection error'));
