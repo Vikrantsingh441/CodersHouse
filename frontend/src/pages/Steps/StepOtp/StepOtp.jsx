e5ea9dbeb9473c7ae819306b7449116001ca8ba6
Vikrant <Singvikrant21@gmail.com>
2024-02-29 12:59:45 +0530

    Update StepOtp.jsx

--- frontend/src/pages/Steps/StepOtp/StepOtp.jsx
+++ frontend/src/pages/Steps/StepOtp/StepOtp.jsx
@@ -3,17 +3,34 @@ import Button from '../../../components/shared/Button/Button'
 import Card from '../../../components/shared/Card/Card'
 import TextInput from '../../../components/shared/TextInput/TextInput'
 import styles from './StepOtp.module.css'
+import {verifyotp} from '../../../http/index'
+import {useSelector, useDispatch} from 'react-redux'
+import {setAuth} from '../../../store/authSlice'
 
-const StepOtp = ({ onNext }) => {
+
+const StepOtp = () => {
+  const dispatch = useDispatch()
+  const {phone,hash} = useSelector((state)=>(state.auth.otp))
   const [otp, setOtp] = useState("")
+  async function submit(){
+    try {
+      const {data} = await verifyotp({otp,phone,hash})
+      dispatch(setAuth({user:data.user}))
+      // onNext()
+      
+    } catch (error) {
+      console.log(error)
+      
+    }
+  }
   return (
     <>
       <div className={styles.cardWrapper}>
         <Card title={"Enter the code we just texted you !"} icon={"lock-emoji"}>
-          <TextInput value={otp} onchange={(e) => setOtp(e.target.value)} />
+          <TextInput value={otp} onChange={(e) => setOtp(e.target.value)} />
           <div>
             <div className={styles.actionButtonWrap}>
-              <Button text={"Next"}></Button>
+              <Button text={"Next"} onClick={submit}></Button>
             </div>
             <p className={styles.bottomParagraph}>
               By entering your number, you're agreeing to our Terms of
